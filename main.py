2ffcdd7c908e779a7eaed46787f16ceb959a4cbc
fraqve <151777628+fraqve@users.noreply.github.com>
2026-03-13 20:14:44 +0100

    Added in pipeline virus total abuseipdb gemini

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
+import analysis
 import scan
 import report
+
+
 
 
 
@@ -9,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("--target","-t",type=str,help="Ip adress",required=True)
     parser.add_argument("--wordlist","-w",type=str,help="Custom wordlist",default="/usr/share/seclists/common.txt")
     args = parser.parse_args()
-
+    config = analysis.load_config("config.json")
 
 
     print(f"[i] Target locked {args.target}")
@@ -18,6 +21,8 @@ if __name__ == "__main__":
     print("Starting nmap scan please wait ....")
     nmap = scan.run_nmap(args.target)
     print("Done!\n")
+    
+    
 
     print("Starting gobuster scan please wait ....")
     gobuster = scan.run_gobuster(args.target,args.wordlist)
@@ -27,4 +32,30 @@ if __name__ == "__main__":
     nikto = scan.run_nikto(args.target)
     print("Done!\n")
 
-    report.gen_report(args.target,nmap,gobuster,nikto)
+    print("Starting virus total scan please wait ....")
+    virustotal = analysis.check_virustotal(args.target,config["VIRUSTOTAL_API_KEY"])
+    print("Done!\n")
+
+
+    print("Starting abuseipdb scan please wait ....")
+    abuseipdb = analysis.check_abuseipdb(args.target,config["ABUSEDB_API_KEY"])
+    print("Done!\n")
+
+    print("Generating master report ...")
+    report.gen_report(args.target,nmap,gobuster,nikto,virustotal,abuseipdb)
+    print("Done\n")
+
+    with open("report.txt","r") as file:
+        master_report = file.read()
+
+    print("Sending to Gemini for analysis please wait ....")
+    gemini_report = analysis.ask_gemini(master_report,config["GEMINI_API_KEY"])
+    print("Done!\n")
+
+    print("Report saved in report.txt")
+    print("Gemini report saved in gemini_report.txt")
+
+    with open("gemini_report.txt","w") as file:
+        file.write(gemini_report)
+
+
